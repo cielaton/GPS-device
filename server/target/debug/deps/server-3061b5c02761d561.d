ad812c285c530530597fcb2699510ab8c81556bb
kartorias1 <vuwin24680@gmail.com>
2024-05-16 14:21:52 +0700

    created server folder

---
+++ server/target/debug/deps/server-3061b5c02761d561.d
@@ -0,0 +1,5 @@
+/home/kiet/projects/working/React Native/gps_device/server/target/debug/deps/libserver-3061b5c02761d561.rmeta: src/main.rs
+
+/home/kiet/projects/working/React Native/gps_device/server/target/debug/deps/server-3061b5c02761d561.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/deps/server-ae0b5be22c6e9d85.d
@@ -0,0 +1,5 @@
+/home/kiet/projects/working/React Native/gps_device/server/target/debug/deps/libserver-ae0b5be22c6e9d85.rmeta: src/main.rs
+
+/home/kiet/projects/working/React Native/gps_device/server/target/debug/deps/server-ae0b5be22c6e9d85.d: src/main.rs
+
+src/main.rs:
